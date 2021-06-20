5c33efbb8f99c54a669c44643b8cf654903790ad
Garrett Quintero <garrett.quintero@gmail.com>
2021-06-20 17:51:38 -0500

    created Guest class

---
+++ src/Guest.java
@@ -0,0 +1,72 @@
+
+public class Guest {
+    public class Reservation {
+        private long roomNumber;
+        private long startTimestamp;
+        private long endTimestamp;
+
+        public long getRoomNumber() {
+            return roomNumber;
+        }
+
+        public void setRoomNumber(long roomNumber) {
+            this.roomNumber = roomNumber;
+        }
+
+        public long getStartTimestamp() {
+            return startTimestamp;
+        }
+
+        public void setStartTimestamp(long startTimestamp) {
+            this.startTimestamp = startTimestamp;
+        }
+
+        public long getEndTimestamp() {
+            return endTimestamp;
+        }
+
+        public void setEndTimestamp(long endTimestamp) {
+            this.endTimestamp = endTimestamp;
+        }
+    }
+
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    // assuming one reservation per guest
+    private Reservation reservation;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+
+}
