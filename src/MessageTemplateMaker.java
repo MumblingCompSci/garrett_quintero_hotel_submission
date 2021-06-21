819ab8b30b4005feee044b4634c701fb4b0d4429
Garrett Quintero <garrett.quintero@gmail.com>
2021-06-20 20:55:47 -0500

    started menu for main program

--- src/MessageTemplateMaker.java
+++ src/MessageTemplateMaker.java
@@ -1,7 +1,47 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class MessageTemplateMaker {
 
-    public static void main(String[] args) {
+    GuestLoader guestLoader;
+    CompanyLoader cLoader;
 
+    // TODO: public void loadGuests()
+    // TODO: public void loadCompanies()
+    // TODO: public void loadTemplates()
+    // TODO: public void newTemplate()
+    // TODO: public void generate()
+
+    public static void main(String[] args) {
+        boolean exit = false;
+
+        String menu = """
+                Please select from the following options\r
+                \t load-guests\r
+                \t load-companies\r
+                \t load-templates\r
+                \t new-template\r
+                \t generate-message\r
+                \t exit\r
+                """;
+
+        while(!exit) {
+            Scanner scan = new Scanner(System.in);
+            System.out.print(menu);
+            switch(scan.nextLine().toLowerCase(Locale.ROOT)) {
+                case "load-guests" :
+                case "load-companies" :
+                case "load-templates" :
+                case "new-template" :
+                case "generate-message" :
+                case "exit" :
+                    exit = true;
+                    break;
+                default :
+                    System.out.println("Oops! That wasn't an option...");
+                    break;
+            }
+        }
 
     }
 }
